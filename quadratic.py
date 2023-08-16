fbe06d7b6bcee0beabb760c37c5aa00e2e53f876
RichA <rich_ay@sbcglobal.net>
2023-08-15 21:17:15 -0500

    Updated the quadratic equation solver.

--- quadratic.py
+++ quadratic.py
@@ -1,15 +1,18 @@
 # Quadratic equation solver.
+# This routine solves for the roots (x) of the equation:
+# ax^2 + bx + c = 0
 
 import math      # import the 'math' module
 
-print( 'This program solves for the roots of a quadriatic equation' )
-print( 'of the form "ax^2 + bx + c"' )
+print( 'This program solves for the roots of a quadratic equation' )
+print( 'of the form "ax^2 + bx + c = 0"' )
 
 a = int( input( 'Enter the coefficient "a": ' ) )
 b = int( input( 'Enter the coefficient "b": ' ) )
 c = int( input( 'Enter the coefficient "c": ' ) )
 
 temp = (b * b) - 4 * a * c
+
 if( temp < 0 ):
     print( 'Sorry, the roots of this quadratic are "imaginary".' )
 else:

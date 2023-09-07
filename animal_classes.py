6c67d66501749120e7da77466fe8edc50de42a94
RichA <rich_ay@sbcglobal.net>
2023-09-07 13:27:21 -0500

    Added additional classes to 'animal_classes.py'.

--- animal_classes.py
+++ animal_classes.py
@@ -19,6 +19,10 @@ class Animal:
         print( " " )
         return
 
+    def move( self ):                          # This method is intended to be overridden by the child classes.
+        print( "No movement specified for this farm animal." )
+        return
+
 
 # Define the various child classes.
 class Horse( Animal ):
@@ -30,8 +34,12 @@ class Horse( Animal ):
     def report( self, a_type = 'Horse' ):
         super().report(a_type)                  # Call the 'report' method of the parent class.
 
+    def move( self, movement ):
+        print( f"This horse is {movement}. " )
+        return
 
-class Cow( Animal ):
+
+class Cow( Animal ):                            # Note this class doesn't have a "move" method!
 
     def speak( self, sound = 'Moo' ):
         super().speak( sound )                  # Call the 'speak' method of the parent class.
@@ -39,6 +47,7 @@ class Cow( Animal ):
     
     def report( self, a_type = 'Cow' ):
         super().report(a_type)                  # Call the 'report' method of the parent class.
+
 
 class Chicken( Animal ):
 
@@ -50,24 +59,34 @@ class Chicken( Animal ):
         super().report(a_type)                  # Call the 'report' method of the parent class.
 
 
+    def move( self, movement ):
+        print( f"This chicken is {movement}. " )
+        return
+
 
 # Main program, create some farm animals ard report them.
 horse1 = Horse( 'Max', 4, 'transportation', 4 )
 horse1.report()
+horse1.move( 'trotting' )
 horse1.speak()
 
 cow1 = Cow( 'Sally', 2, 'food', 4 )
 cow1.report()
+cow1.move(  )                                    # Invokes the parent class's 'move' method.
 cow1.speak()
 
 chick1 = Chicken( 'Dodo', 3, 'food & eggs', 2 )
 chick1.report()
+chick1.move( 'meandering' )
 chick1.speak()
 
 # Display a few more 'internal' details.
+
+print( "'chick1' is: ", type(chick1) )
+print( chick1 )
+
 print( "'horse1' is: ", type(horse1) )
 print( horse1 )
-
 
 # Display details on the 'objects'.
 print( "For the 'horse1' object ... " )

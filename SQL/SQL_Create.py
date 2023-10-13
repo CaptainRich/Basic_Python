6e81ad7dfdfa1ba78d94973e65a40ddcc7d6dd14
RichA <rich_ay@sbcglobal.net>
2023-10-12 21:36:57 -0500

    Finalized 'sql_create.py'.

--- SQL/SQL_Create.py
+++ SQL/SQL_Create.py
@@ -1,6 +1,20 @@
 # This routine illustrates creating and Querying a database.
 
-########################################################################################33
+#########################################################################################
+def dump_db( db_file ):
+    """Function to dump out all the records of the database."""
+
+    with sqlite3.connect( db_file ) as connection:
+        cursor = connection.cursor()
+
+        cursor.execute(
+            "SELECT Name, Species, Age FROM Roster;"
+        )
+
+        for row in cursor.fetchall():
+            print( row )
+
+##########################################################################################
 # Main
 
 import sqlite3
@@ -39,13 +53,36 @@ with sqlite3.connect( db_file ) as connection:
     cursor.executemany( "INSERT INTO Roster VALUES(?, ?, ?);", seed_values )
 
 # The database is created and populated.  Dump out all of the rows.
+print( "\nInitial database:" )
+dump_db( db_file )
+
+# Now update the name of 'Jadzia Dax' to be 'Ezri Dax'.   We'll reopen the 
+# database for this operation (rather inefficient but ...)
+
+update_name = """
+    UPDATE Roster SET Name='Ezri Dax' WHERE Name='Jadzia Dax'
+"""
 
 with sqlite3.connect( db_file ) as connection:
     cursor = connection.cursor()
+    cursor.execute( update_name )
 
-    cursor.execute(
-        "SELECT Name, Species, Age FROM Roster;"
-    )
+# Dump the database again to view the change.
+print( "\nDatabase after updating Jadzia to Ezri:" )
+dump_db( db_file )
+
+# Now display the name and age of everyone classified as 'Bajoran'.   We'll reopen the 
+# database for this operation (rather inefficient but ...)
+
+show_bajoran = """
+    SELECT Name, Age FROM Roster WHERE Species='Bajoran'
+"""
+
+print( "\nAll rows where 'species=Bajoran': " )
+with sqlite3.connect( db_file ) as connection:
+    cursor = connection.cursor()
+    cursor.execute( show_bajoran )
 
     for row in cursor.fetchall():
         print( row )
+

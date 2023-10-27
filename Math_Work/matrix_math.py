f5c3727a403a57fb6b0f58a41debb8f699a0e566
RichA <rich_ay@sbcglobal.net>
2023-10-26 22:06:39 -0500

    Updated matrix_math.py

--- Math_Work/matrix_math.py
+++ Math_Work/matrix_math.py
@@ -7,7 +7,10 @@ import numpy as np
 
 # Define a 3*3 (2D) matrix, literally
 m2d = np.array( [ [1,2,3], [4,5,6], [7,8,9] ] )  # Three rows of 3 columns
+n2d = np.arange( 11, 20 )
+n2d = n2d.reshape( 3, 3 )
 print( "\nThe m2d array is:\n", m2d )
+print( "\nThe matrix n2d is:\n", n2d )
 
 print( "\nThe middle value of the middle row is: ", m2d[1,1] )
 
@@ -20,3 +23,11 @@ m3d = np.array ([
 print( "\nThe m3d array is:\n", m3d )
 print( "\nThe shape of m3d is: ", m3d.shape )
 print( "\nThe middle value of the middle row of the 2nd plane is: ", m3d[1,1, 1] )
+
+m3d = m3d * 2
+print( "\nm3d times 2 is:\n", m3d )
+
+# Multiply m2d by n2d
+mn2d = np.matmul( m2d, n2d )
+print( "\nThe result of multiplying m2d*n2d is:\n" )
+print( mn2d )

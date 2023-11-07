21b09bda2b61bf51325d8296f48c3d14570ff52f
RichA <rich_ay@sbcglobal.net>
2023-11-07 09:53:46 -0600

    Updated 'Tkinter_test.py' to use 'frame' and '.place'.

--- GUI_Work/TKinter_test.py
+++ GUI_Work/TKinter_test.py
@@ -10,14 +10,18 @@ def get_response():
 ############################################################
 
 window = tk.Tk()            # Create the dialog window
-label  = tk.Label( text='Enter your name:', bg='black', fg='yellow' )
-label.pack()                # Put the label on the dialog
-entry  = tk.Entry()
-entry.pack()                # Put the text entry box on the dialog
+frame1 = tk.Frame( master=window, width=200, height=150 )         # Create a frame to hold the widgets
+frame1.pack(  )
+
+label  = tk.Label( master=frame1, text='Enter your name:', bg='black', fg='yellow' )
+label.place( x=35, y=20 )               # Put the label on the dialog, location is in pixels
+entry  = tk.Entry( master=frame1 )
+entry.place( x=20, y=50 )                 # Put the text entry box on the dialog
 
 
-submit_btn = tk.Button( text='Submit', command=get_response )
-submit_btn.pack()
+submit_btn = tk.Button(  master=frame1, text='Submit', relief=tk.RAISED, command=get_response )
+submit_btn.place( x=55, y=80 )
+
 
 
 window.mainloop()

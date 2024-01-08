baefe559f3fc041ef1b4d08cac6dbfdcba8e9abe
RichA <rich_ay@sbcglobal.net>
2024-01-07 22:19:20 -0600

    Refactoring, added 'readme.md'.

---
+++ Class_Work/README.md
@@ -0,0 +1,23 @@
+# $`\textcolor{blue}{\text{Python Basics - GUI Work}}`$
+A collection of Python (.py) files illustrating  Python concepts in dealing 
+with the a Classes.  
+Richard Ay (January 2024)
+
+## $`\textcolor{blue}{\text{Table of Contents}}`$  
+* [Setup](#setup)
+* [File List](#file-list)
+
+
+## Setup
+Refer to the main 'readme.md' in the directory above.  There is no special setup required for
+the scripts in this subdirectory.  
+
+
+## File List
+**cars.py** - a script to monitor and report on "car" statistics.  
+
+**class_package** - a package of .py files/classes to facilitate the scripts in this subdirectory.
+
+ 
+ 
+
--- Class_Work/cars.py
+++ Class_Work/cars.py
@@ -2,38 +2,54 @@
 
 import class_package.car_classes as crs
 
-# Instantiate several cars.
+#############################################################################
+# Setup the cars
 
-taycan = crs.Car( 
-    'Porsche', 'Taycan', 2020, 'Electric', 'White', 'all-wheel',
-    17100, 0, 0, '9/2024', 'NSS 9353', 'HCTR1-1697792' )
+def set_cars():
 
-p911 = crs.Car( 
-    'Porsche', '911', 2008, 'Gasoline', 'Baltic Blue', 'all-wheel',
-     9925, 9721, 6700, '5/2024', 'HSM 610', 'HCTRA03615043' )
+    # Instantiate several cars.
 
-tesla = crs.Car( 
-    'Tesla', 'S-P100D', 2017, 'Electric', 'White', 'all-wheel',
-     55000, 0, 6700, '4/2024', 'SUVIVR', 'HCTR08587921' )
+    # Make, Model, Year, Engine-Type, Color, Drive-Type
+    # Mileage, Oil-Miles, Tire-Miles, Inspection, License, Toll-Tag
 
-yukon = crs.Car(
-    'GMC', 'Yukon XL', 2017, 'Gasoline', 'Dark Gray', 'rear-wheel',
-    70000, 58616, 30867, '5/2024', 'LYD 0892', 'HCTRA10668298' )
+    taycan = crs.Car( 
+        'Porsche', 'Taycan', 2020, 'Electric', 'White', 'all-wheel',
+        17100, 0, 0, '9/2024', 'NSS 9353', 'HCTR1-1697792' )
 
-sl = crs.Car(
-    'Mercedes', 'SL65 AMG', 2007, 'Gasoline', 'Capri Blue', 'rear-wheel',
-    28600, 25035, 18000, '2/2024', 'GWL 9350', 'HCTRA03830620' )
+    p911 = crs.Car( 
+        'Porsche', '911', 2008, 'Gasoline', 'Baltic Blue', 'all-wheel',
+         9925, 9721, 6700, '5/2024', 'HSM 610', 'HCTRA03615043' )
 
-a5 = crs.Car(
-    'Audi', 'A5', 2012, 'Gasoline', 'White', 'rear-wheel',
-    110000, 90000, 0, '9/2024', 'CRP 6302', 'HCTRA06418388' )
+    tesla = crs.Car( 
+        'Tesla', 'S-P100D', 2017, 'Electric', 'White', 'all-wheel',
+         55000, 0, 6700, '4/2024', 'SUVIVR', 'HCTR08587921' )
 
-arteon = crs.Car(
-    'Volkswagen', 'Arteon', 2022, 'Gasoline', 'White', 'rear-wheel',
-    46000, 40000, 450000, '2/2024', 'RTZ 0525', 'HCTR13111680' )
+    yukon = crs.Car(
+        'GMC', 'Yukon XL', 2017, 'Gasoline', 'Dark Gray', 'rear-wheel',
+        70000, 58616, 30867, '5/2024', 'LYD 0892', 'HCTRA10668298' )
+
+    sl = crs.Car(
+        'Mercedes', 'SL65 AMG', 2007, 'Gasoline', 'Capri Blue', 'rear-wheel',
+        28600, 25035, 18000, '2/2024', 'GWL 9350', 'HCTRA03830620' )
+
+    a5 = crs.Car(
+        'Audi', 'A5', 2012, 'Gasoline', 'White', 'rear-wheel',
+        110000, 90000, 0, '9/2024', 'CRP 6302', 'HCTRA06418388' )
+
+    arteon = crs.Car(
+        'Volkswagen', 'Arteon', 2022, 'Gasoline', 'White', 'rear-wheel',
+        46000, 40000, 450000, '2/2024', 'RTZ 0525', 'HCTR13111680' )
+    
+    return taycan, p911, tesla, yukon, sl, a5, arteon
 
 
-# REport on the information specified above.
+###########################################################################
+# Main
+    
+# Setup the cars
+taycan, p911, tesla, yukon, sl, a5, arteon = set_cars()
+
+# Report on the information specified above.
 
 taycan_info = taycan.report_car()
 print( "\n", taycan_info )

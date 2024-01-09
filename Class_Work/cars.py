63e13fb60487ca6886cffdf1d643d78843f2d91a
RichA <rich_ay@sbcglobal.net>
2024-01-09 17:36:41 -0600

    Finished sub-classes for fossil and electric cars.

--- Class_Work/cars.py
+++ Class_Work/cars.py
@@ -10,42 +10,42 @@ def set_cars():
     # Instantiate several cars.
 
     # Make, Model, Year, Engine-Type, Color, Drive-Type
-    # Mileage, Oil-Miles, Tire-Miles, Inspection, License, Toll-Tag
+    # Mileage, Tire-Miles, Inspection, License, Toll-Tag,
+    # (Oil-Miles or Battery Range)
 
-    taycan = crs.Car( 
+    taycan = crs.Ec_Car( 
         'Porsche', 'Taycan', 2020, 'Electric', 'White', 'all-wheel',
-        17100, 0, 0, '9/2024', 'NSS 9353', 'HCTR1-1697792' )
+        17100, 0, '9/2024', 'NSS 9353', 'HCTR1-1697792', 246 )
 
-    p911 = crs.Car( 
+    p911 = crs.Ff_Car( 
         'Porsche', '911', 2008, 'Gasoline', 'Baltic Blue', 'all-wheel',
-         9925, 9721, 6700, '5/2024', 'HSM 610', 'HCTRA03615043' )
+         9925, 9721, '5/2024', 'HSM 610', 'HCTRA03615043', 6700 )
 
-    tesla = crs.Car( 
+    tesla = crs.Ec_Car( 
         'Tesla', 'S-P100D', 2017, 'Electric', 'White', 'all-wheel',
-         55000, 0, 6700, '4/2024', 'SUVIVR', 'HCTR08587921' )
+         55000, 6700, '4/2024', 'SUVIVR', 'HCTR08587921', 276 )
 
-    yukon = crs.Car(
+    yukon = crs.Ff_Car(
         'GMC', 'Yukon XL', 2017, 'Gasoline', 'Dark Gray', 'rear-wheel',
-        70000, 58616, 30867, '5/2024', 'LYD 0892', 'HCTRA10668298' )
+        65714, 58616, '5/2024', 'LYD 0892', 'HCTRA10668298', 30867 )
 
-    sl = crs.Car(
+    sl = crs.Ff_Car(
         'Mercedes', 'SL65 AMG', 2007, 'Gasoline', 'Capri Blue', 'rear-wheel',
-        28600, 25035, 18000, '2/2024', 'GWL 9350', 'HCTRA03830620' )
+        28600, 18000, '2/2024', 'GWL 9350', 'HCTRA03830620', 25035 )
 
-    a5 = crs.Car(
+    a5 = crs.Ff_Car(
         'Audi', 'A5', 2012, 'Gasoline', 'White', 'rear-wheel',
-        110000, 90000, 0, '9/2024', 'CRP 6302', 'HCTRA06418388' )
+        110000, 0, '9/2024', 'CRP 6302', 'HCTRA06418388', 90000 )
 
-    arteon = crs.Car(
+    arteon = crs.Ff_Car(
         'Volkswagen', 'Arteon', 2022, 'Gasoline', 'White', 'rear-wheel',
-        46000, 40000, 450000, '2/2024', 'RTZ 0525', 'HCTR13111680' )
+        46000, 450000, '2/2024', 'RTZ 0525', 'HCTR13111680', 40000 )
     
     # Return the 'car objects' in a 'list'
     cars=[taycan, p911, tesla, yukon, sl, a5, arteon]
     
     
     return cars
-
 
 ###########################################################################
 # Main
--- Class_Work/class_package/car_classes.py
+++ Class_Work/class_package/car_classes.py
@@ -1,11 +1,11 @@
-""" Class structure for the 'restaurant' exercise."""
+""" Class structure for the 'car' objects."""
 
 class Car:
 
     def __init__ ( self, make, model, year, engine, color, drive_train,
-                   mileage, oil_change_miles, tire_miles, 
-                   inspection_date, plate, tag ):
-        """Initialize the attributes for a car."""
+                   mileage, tire_miles, inspection_date, 
+                   plate, tag ):
+        """Initialize the attributes for a general car."""
         self.make             = make
         self.model            = model
         self.year             = year
@@ -13,20 +13,18 @@ class Car:
         self.color            = color
         self.drive_train      = drive_train
         self.mileage          = mileage
-        self.oil_change_miles = oil_change_miles
         self.tire_miles       = tire_miles
         self.inspection_date  = inspection_date
         self.license_plate    = plate
-        self.toll_tag         = tag
+        self.toll_tag         = tag        
 
     def report_car( self ):
-        """ Report all of the information for a car. """
+        """ Report all of the information for a general car. """
         car_info = f"{self.year} {self.make} {self.model} \n" \
                    + f" Engine Type: {self.engine} \n" \
                    + f" Color: {self.color} \n" \
                    + f" Drive Train: {self.drive_train} \n" \
                    + f" Odometer: {self.mileage} \n" \
-                   + f" Oil Changed at (miles): {self.oil_change_miles} \n" \
                    + f" Tires Changed at (miles): {self.tire_miles} \n" \
                    + f" Inspection Date: {self.inspection_date} \n" \
                    + f" License Plate: {self.license_plate} \n" \
@@ -65,4 +63,56 @@ class Car:
         """ Update the date of the last inspection for the car. """
 
         self.inspection_date = new_date
-      
+
+################################################################################
+
+""" Subclasses to differentiate electric cars from fossil fuel cars. """
+
+# The Electric car class.
+class Ec_Car( Car ):
+
+    def __init__ ( self, make, model, year, engine, color, drive_train,
+                   mileage, tire_miles, inspection_date, 
+                   plate, tag, max_range ):
+         """ Initialize the attributes for an electric car. """ 
+         self.max_range = max_range
+
+         # Initialize the remaining attributes of the 'Car' class.
+         super().__init__( make, model, year, engine, color, drive_train,
+                   mileage, tire_miles, 
+                   inspection_date, plate, tag )
+         
+    def report_car( self ):
+        """" Get the car's information from the parent class. """
+        car_info = super().report_car( )
+        car_info += f" Max Range:  {self.max_range} \n" # add EC_Car specifics
+
+        return car_info
+
+
+################################################################################
+
+""" Subclasses to differentiate fossil fuel cars from electric cars. """
+
+# The Fossil Fuel car class.
+class Ff_Car( Car ):
+
+    def __init__ ( self, make, model, year, engine, color, drive_train,
+                   mileage, tire_miles, inspection_date,
+                    plate, tag, oil_change_miles ):
+         
+         """ Initialize the attributes for a Fossil fuel car. """ 
+         self.oil_change_miles = oil_change_miles
+
+         # Initialize the remaining attributes of the 'Car' class.
+         super().__init__( make, model, year, engine, color, drive_train,
+                   mileage, tire_miles, inspection_date, plate, tag )
+         
+    def report_car( self ):
+        """" Get the car's information from the parent class. """
+        car_info = super().report_car( )
+
+        # Add Fossil fuel car specifics
+        car_info += f" Oil Changed at (miles): {self.oil_change_miles} \n"
+
+        return car_info

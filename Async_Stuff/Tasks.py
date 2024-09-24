54c3bd2465dfafa3351e0100fc48aed754d970e1
RichA <rich_ay@sbcglobal.net>
2024-09-24 17:07:11 -0500

    Added tasks.py

---
+++ Async_Stuff/Tasks.py
@@ -0,0 +1,22 @@
+""" An illustration of asynchronous tasks,  Tasks are used to
+    schedule and run coroutines concurrently. """
+
+import asyncio
+
+async def count( n, taskId ):
+    for i in range( n ):
+        print( "TaskID: ", taskId, "  i: ", i )
+        await asyncio.sleep( 1 )
+
+
+async def main():
+    # Because of the "async", tasks 1 and 2 run simultaneously, each
+    # printing an iteration in turn.
+    task1 = asyncio.create_task( count( 5, 'TASK1' ) )
+    task2 = asyncio.create_task( count( 3, '   task2' ) )
+
+    await task1
+    await task2
+
+
+asyncio.run( main() )

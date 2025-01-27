df4204e32467b23a182cfd9738c14f95537c70a5
RichA <rich_ay@sbcglobal.net>
2025-01-27 16:49:47 -0600

    Updated the 'queues.py' script with a sentinel value.

--- Async_Stuff/queues.py
+++ Async_Stuff/queues.py
@@ -10,10 +10,17 @@ async def producer( queue ):
         print( f"Produced {i}" )
         await asyncio.sleep( 2 )
 
+    await queue.put( None ) # A sentinel value to signal 'consumer' to stop
+
 async def consumer( queue ):
     # The function that consumes data from the queue.
     while True:
         item = await queue.get()  # get an item from the queue, wait if needed
+
+        if item is None :         # check for the sentinel value
+            queue.task_done()
+            break                 # exit the 'while' loop
+
         print( f"Consumed {item}" )
         queue.task_done()
 
@@ -21,14 +28,14 @@ async def consumer( queue ):
 
 async def main():
     queue         = asyncio.Queue()
+    # The 'create_task' function schedules these tasks for execution
     producer_task = asyncio.create_task( producer( queue ) )
     consumer_task = asyncio.create_task( consumer( queue ) )
 
-    # 'gather' runs both coroutines concurrently.  Note that if 'consumer_task'
-    # is added to the 'gather' method, this script hangs???
-    await asyncio.gather( producer_task, return_exceptions=True )
+    # 'gather' runs both coroutines concurrently.  
+    await asyncio.gather( producer_task, consumer_task, return_exceptions=True )
 
-    await queue.join() # ensure all items are processed before exiting
+    await queue.join() # ensure all items are processed by 'consumer_task' before exiting
 
 
 asyncio.run( main() )
